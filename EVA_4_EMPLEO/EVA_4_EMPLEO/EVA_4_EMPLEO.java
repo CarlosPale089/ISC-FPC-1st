a7a10725f0e473cec5abe7cee8a7c42a284272a9
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-08-27 21:54:21 -0600

    Add files via upload

---
+++ EVA_4_EMPLEO/EVA_4_EMPLEO/EVA_4_EMPLEO.java
@@ -0,0 +1,17 @@
+
+package eva_4_empleo;
+
+public class EVA_4_EMPLEO {
+
+    public static void main(String[] args) {
+   System.out.print("Empleo: ");
+   System.out.println("Doctor");
+  System.out.print("Salario: ");
+          System.out.println("20000");
+                  System.out.print("Horas semanales: ");
+                  System.out.println("400");
+                  System.out.print("Requiere ingles: ");
+                  System.out.println(false);
+    }
+    
+}
---
+++ EVA_4_EMPLEO/EVA_4_EMPLEO/src/eva_4_empleo/EVA_4_EMPLEO.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_4_empleo;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA_4_EMPLEO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   System.out.print("Empleo: ");
+   System.out.println("Doctor");
+  System.out.print("Salario: ");
+          System.out.println("20000");
+                  System.out.print("Horas semanales: ");
+                  System.out.println("400");
+                  System.out.print("Requiere ingles: ");
+                  System.out.println(false);
+    }
+    
+}
