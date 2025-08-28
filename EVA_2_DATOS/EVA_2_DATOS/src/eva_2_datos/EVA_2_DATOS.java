683e7d2d0d3156c33b169682021129ab8351b0c9
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-08-27 21:18:51 -0600

    Add files via upload

---
+++ EVA_2_DATOS/EVA_2_DATOS/src/eva_2_datos/EVA_2_DATOS.java
@@ -0,0 +1,13 @@
+
+package eva_2_datos;
+
+public class EVA_2_DATOS {
+
+  
+    public static void main(String[] args){
+     System.out.println("CARLOSCASTILLO");
+     System.out.println(100);
+    System.out.println(52.3);
+    System.out.println(true);}
+}
+   
