b388959ec028d9a98c32053e8f0ff8e730fe27b3
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-08-27 22:03:48 -0600

    Add files via upload

---
+++ EVA_1_HOLA_MUNDO/EVA_1_HOLAMUNDO.java
@@ -0,0 +1,11 @@
+
+package eva_1_holamundo;
+
+public class EVA_1_HOLAMUNDO {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Fundamentos de la programación");
+    }
+    
+}
