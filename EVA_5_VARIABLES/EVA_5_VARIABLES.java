31d0a576d83b76673d3f668c0a04dc11e1a6c544
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-08-31 14:10:52 -0600

    Add files via upload

---
+++ EVA_5_VARIABLES/EVA_5_VARIABLES.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_5_variables;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA_5_VARIABLES {
+
+
+    public static void main(String[] args) {
+       //DECLARAR LAS VARIABLES
+       //TIPO DE DATO   NOMBRE DEL IDENTIFICADOR
+        String Nombre;
+        int Edad;
+        double Salario;
+        //INICIALIZACION:
+        //IDENTIFICADOR = VALOR;
+        // --> OPERADOR DE ASIGNACIÓN
+        Nombre = "CARLOS DAVID CASTILLO GAYTAN";
+        Edad = 17;
+        Salario = 3.5;
+        //EDAD = "CARLOS"
+        System.out.println(Nombre);
+         System.out.println(Salario);
+          System.out.println(Edad);
+    }
+    
+}
