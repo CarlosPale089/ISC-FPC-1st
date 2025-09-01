2699274cbd0e1d9337817fc5b0da4a252a795521
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-09-01 16:45:40 -0600

    Add files via upload

---
+++ EVA_6_OPERACIIONES/EVA_6_OPERACIONES/src/eva_6_operaciones/EVA_6_OPERACIONES.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_6_operaciones;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA_6_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     double pi;
+     double radio;
+     double area;
+     pi = 3.1416;
+     radio = 10;
+     area = pi * radio * radio;
+     System.out.println("El area del circulo de radio 10 es:");
+     System.out.println(area);
+     
+    }
+    
+}
