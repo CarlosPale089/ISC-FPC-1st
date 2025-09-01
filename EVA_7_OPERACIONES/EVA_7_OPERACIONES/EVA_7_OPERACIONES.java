3263a099737d816cd383428b06084af10d1a24a4
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-09-01 16:46:17 -0600

    Add files via upload

---
+++ EVA_7_OPERACIONES/EVA_7_OPERACIONES/EVA_7_OPERACIONES.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_7_operaciones;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA_7_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      double base;
+      double altura;
+      double area;
+      base = 12;
+      altura = 17;
+      area = base * altura / 2;
+      System.out.println("El area del triangulo de base 12 es:");
+      System.out.println(area);
+    }
+    
+}
