f200f194dd4fa832233dd1e3cafecdeaeae11b79
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-09-01 22:43:46 -0600

    Add files via upload

---
+++ EVA_8_TEMPERATURAS/EVA_8_TEMPERATURAS/src/eva_8_temperaturas/EVA_8_TEMPERATURAS.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_8_temperaturas;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA_8_TEMPERATURAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double farenheith;
+        double celsius;
+        farenheith = 70; 
+        celsius = farenheith - 32 * 5 / 9;
+        System.out.println("Los grados celsius son");
+        System.out.println(celsius);
+        
+        
+        
+    }
+    
+}
