e42b6a6fda8448cf2211e2e2920b848170a1ae03
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-09-08 20:27:27 -0600

    Add files via upload

---
+++ EVA_12_IF/EVA_12_IF.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_12_if;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA_12_IF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int calificación;
+       Scanner input = new Scanner(System.in);
+      
+         System.out.println("Introduce tu calificación: ");
+                calificación = input.nextInt();
+
+
+        if (calificación >= 70) {System.out.println("Acredito");
+        } else { System.out.println("No acredito");
+        }
+
+        
+    
+   
+        
+       
+          
+       
+        
+    
+       
+    }
+    
+}
