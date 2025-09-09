0eb013946eef715bc36c6a3ea9763ab9407e055d
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-09-08 20:28:04 -0600

    Add files via upload

---
+++ EVA_13_IF/EVA_13_IF/src/eva_13_if/EVA_13_IF.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_13_if;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA_13_IF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int edad;
+       Scanner input = new Scanner (System.in);
+       edad = input.nextInt();
+       if(edad < 18) {System.out.println("Puede comprar alcohol");
+        } else {System.out.println("Puede comprar alcohol");
+           
+        }
+       
+       
+       
+       
+    }
+    
+}
