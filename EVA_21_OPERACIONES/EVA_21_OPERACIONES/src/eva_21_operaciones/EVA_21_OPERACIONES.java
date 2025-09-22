e8e8d2774d5f36a01c8c7b18df82d49be6a87582
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-09-21 22:22:47 -0600

    Add files via upload

---
+++ EVA_21_OPERACIONES/EVA_21_OPERACIONES/src/eva_21_operaciones/EVA_21_OPERACIONES.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_21_operaciones;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA_21_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int valor = 2, expo = 3;
+       double resultado;
+       resultado = Math.pow(valor, expo);
+        System.out.println("2 al cubo = ");
+        System.out.println(resultado);
+        
+        int valor2 = 100;
+        double resultado2;
+        resultado2 = Math.sqrt(valor2);
+        System.out.println("raiz cuadrada de 100 = ");
+        System.out.println(resultado2);
+        
+        int valor3 = 125;
+        double resultado3;
+        resultado3 = Math.pow(valor3, resultado);
+        System.out.println("1/3 es = ");
+        System.out.println(resultado3);
+        
+        
+                
+        
+    }
+    
+    
+}
